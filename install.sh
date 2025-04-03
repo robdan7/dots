2824221b009e5472b7583eb4b1575c9acadf6e83
robyn <danielson.robyn@gmail.com>
2025-04-03 10:27:36 +0200

    Fixed install script

---
+++ install.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/bash
+pacman -S --needed - < pkglist.txt
+
+# Download Git submodules
+git submodule init
+git submodule update
+
+# Set nano as default editor in git
+git config --global core.editor "nano"
+
+# Set fish as default shell
+chsh -s /usr/bin/fish
+
+# Install yay
+cd ./yay
+makepkg -si --noconfirm
+cd ..
+
+# TODO move yay packages to install file
+yay -S --sudoloop --noconfirm --needed - < pkglist.yay.txt
+
+# Install pywal16
+cd ./pywal16
+pipx install .
+pipx ensurepath
+cd ..
+
+# Create symlinks
+ln -s -r $(pwd)/* ~/.config/
+ln -s -r ./.Xresources ~/.Xresources 
+xrdb ~/.Xresources
+
+# Install biscuit theme for fish
+fish -c "fisher install projekt0n/biscuit"
+
+# Generate fastfetch image
+mkdir ~/.cache/chafa -p
+chafa  $(pwd)/chafa/Arch-Linux-UwU-Linux-2036721-4111737064.png -s 60x60 --polite true > ~/.cache/chafa/icon
